14491bd6c240a58197a16e68ea73278cf0ccdb00
Tanujkumar24 <tanuj.mangalapally@gmail.com>
2024-06-13 17:27:05 +0530

    updated requirements

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Tanujkumar",
+    author_email="tanuj.mangalapally@gmail.com",
+    packages=find_packages()
+)
